bf901132bb6d4d78a2f263cdddab06df8cf9d12e
Lucas Ordaz <ordaz.lucas@gmail.com>
2013-09-07 18:26:51 -0400

    Added Welcome Dialog Box
    Added a welcome dialog box that contains button for loading a base sound
    into the application (no functionality just yet). Cleaned up the CSS and
    HTML.

---
+++ demos/pageslide/js/ui-functions.js
@@ -0,0 +1,3 @@
+$(".DialogBox").draggable();
+/* Slide to the left, and make it model (you'll have to call $.pageslide.close() to close) */
+$(".advance-controls").pageslide({ direction: "right", modal: true });
