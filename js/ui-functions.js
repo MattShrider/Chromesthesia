40b5eb21420e6fdb72c19d8815b6dcb34ac07e49
Lucas G. Ordaz <ordaz.lucas@gmail.com>
2013-09-26 18:50:47 -0400

    Created Settings Dialog box, included plugins
    Created the settings dialog box that allows the change of appearance of
    Dialog Boxes (more will be added later). Also included the necessary
    jquery plugins for the controls to work.

--- js/ui-functions.js
+++ js/ui-functions.js
@@ -27,8 +27,165 @@ $("#SampleButton").click(function(){
 	$("#ControlBox").show();
 });
 
+$("#testButton").click(function(){
+	$("#SettingsDialog").show();
+
+});
+
+$("#BackgroundColor").spectrum({
+    color: "#ECC",
+    showInput: true,
+    className: "full-spectrum",
+    showInitial: true,
+    showAlpha: true,
+    maxPaletteSize: 10,
+    preferredFormat: "rgb",
+    localStorageKey: "spectrum.demo",
+    move: function (color) {
+        updateBackgroundColor('body',color);
+    },
+    show: function () {
+    
+    },
+    beforeShow: function () {
+    
+    },
+    hide: function () {
+    
+    },
+    change: function(color) {
+        updateBackgroundColor('body',color);
+    },
+});
+
+$("#DialogFontColor").spectrum({
+    color: "#ECC",
+    showInput: true,
+    className: "full-spectrum",
+    showInitial: true,
+    showAlpha: true,
+    maxPaletteSize: 10,
+    preferredFormat: "hex",
+    localStorageKey: "spectrum.demo",
+    move: function (color) {
+        updateFontColor('.DialogBox',color);
+    },
+    show: function () {
+    
+    },
+    beforeShow: function () {
+    
+    },
+    hide: function () {
+    
+    },
+    change: function(color) {
+        updateFontColor('.DialogBox',color);
+    },
+});
+
+$('#font').fontselect().change(function(){
+        
+          // replace + signs with spaces for css
+          var font = $(this).val().replace(/\+/g, ' ');
+          
+          // split font into family and weight
+          font = font.split(':');
+          
+          // set family on paragraphs 
+          updateFontStyle('.DialogBox', font[0]);
+        });
+
+
+$("#DialogFontSize").on('keyup change click', function () {
+	updateFontSize('.DialogBox', $("#DialogFontSize").val()/2 + "px" )
+	
+});
+
+$("#DialogBGColor").spectrum({
+    color: "#ECC",
+    showInput: true,
+    className: "full-spectrum",
+    showInitial: true,
+    showAlpha: true,
+    maxPaletteSize: 10,
+    preferredFormat: "rgb",
+    localStorageKey: "spectrum.demo",
+    move: function (color) {
+        updateBackgroundColor('.DialogBox',color);
+    },
+    show: function () {
+    
+    },
+    beforeShow: function () {
+    
+    },
+    hide: function () {
+    
+    },
+    change: function(color) {
+        updateBackgroundColor('.DialogBox',color);
+    },
+});
+
+$("#DialogBorderSize").on('keyup change click', function () {
+	updateBorderSize('.DialogBox', $("#DialogBorderSize").val() + "px" )
+	
+});
+$("#DialogBorderRadius").on('keyup change click', function () {
+	updateBorderRadius('.DialogBox', $("#DialogBorderRadius").val() + "px" )
+	
+});
+$("#DialogBorderColor").spectrum({
+    color: "#ECC",
+    showInput: true,
+    className: "full-spectrum",
+    showInitial: true,
+    showAlpha: true,
+    maxPaletteSize: 10,
+    preferredFormat: "rgb",
+    localStorageKey: "spectrum.demo",
+    move: function (color) {
+        updateBorderColor('.DialogBox',color);
+    },
+    show: function () {
+    
+    },
+    beforeShow: function () {
+    
+    },
+    hide: function () {
+    
+    },
+    change: function(color) {
+        updateBorderColor('.DialogBox',color);
+    },
+});
+
 function fileSelected() {
 	$("#WelcomeBox").hide();
 	alert($("#LocalSong").val());
 	$("#ControlBox").show();
+}
+
+function updateBackgroundColor(element, color){
+	$(element).css("background-color", color.toString());
+}
+function updateFontColor(element, color){
+	$(element).css("color", color.toHexString());
+}
+function updateFontSize(element, size) {
+	$(element).css("font-size", size  );   	
+}
+function updateFontStyle(element, style) {
+	$(element).css("font-family", style  );   	
+}
+function updateBorderColor(element, color){
+	$(element).css("border-color", color.toString());
+}
+function updateBorderSize(element, size){
+	$(element).css("border-width", size  );   
+}
+function updateBorderRadius(element, size){
+	$(element).css("border-radius", size  );   	
 }
